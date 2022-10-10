d532ea7a080cc52849718a91e0c91371039a1b53
Arjay Angeles <aqangeles@gmail.com>
2022-10-10 22:25:16 +0800

    chore: ga code

--- resources/views/docs/app.blade.php
+++ resources/views/docs/app.blade.php
@@ -50,7 +50,7 @@
 
 @production
     <script>
-        var _gaq = [['_setAccount', 'UA-23865777-1'], ['_trackPageview']];
+        var _gaq = [['_setAccount', 'UA-63183947-3'], ['_trackPageview']];
         (function (d, t) {
             var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
             g.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
