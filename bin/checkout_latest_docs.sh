a672b19bc1d962361351a5cb8b8c6d686a1b2e7a
Arjay Angeles <aqangeles@gmail.com>
2022-10-10 18:59:07 +0800

    chore: add acl package docs

--- bin/checkout_latest_docs.sh
+++ bin/checkout_latest_docs.sh
@@ -17,3 +17,21 @@ for v in "${DATATABLES[@]}"; do
         git clone --single-branch --branch "$v" git@github.com:yajra/laravel-datatables-docs.git "resources/docs/laravel-datatables/$v"
     fi;
 done
+
+ACL=(
+  master
+  9.0
+  5.0
+  4.0
+  3.0
+)
+
+for v in "${ACL[@]}"; do
+    if [ -d "resources/docs/laravel-acl/$v" ]; then
+        echo "Pulling latest documentation updates for $v..."
+        (cd resources/docs/laravel-acl/$v && git pull)
+    else
+        echo "Cloning $v..."
+        git clone --single-branch --branch "$v" git@github.com:yajra/laravel-acl-docs.git "resources/docs/laravel-acl/$v"
+    fi;
+done
