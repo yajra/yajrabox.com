6fb913c99e0e96f965c28b82a6799080ef730b07
Arjay Angeles <aqangeles@gmail.com>
2022-10-11 14:31:08 +0800

    chore: clone using https

--- bin/checkout_latest_docs.sh
+++ bin/checkout_latest_docs.sh
@@ -14,7 +14,7 @@ for v in "${DATATABLES[@]}"; do
         (cd resources/docs/laravel-datatables/$v && git pull)
     else
         echo "Cloning $v..."
-        git clone --single-branch --branch "$v" git@github.com:yajra/laravel-datatables-docs.git "resources/docs/laravel-datatables/$v"
+        git clone --single-branch --branch "$v" https://github.com/yajra/laravel-datatables-docs "resources/docs/laravel-datatables/$v"
     fi;
 done
 
@@ -32,7 +32,7 @@ for v in "${ACL[@]}"; do
         (cd resources/docs/laravel-acl/$v && git pull)
     else
         echo "Cloning $v..."
-        git clone --single-branch --branch "$v" git@github.com:yajra/laravel-acl-docs.git "resources/docs/laravel-acl/$v"
+        git clone --single-branch --branch "$v" https://github.com/yajra/laravel-acl-docs "resources/docs/laravel-acl/$v"
     fi;
 done
 
@@ -51,7 +51,7 @@ for v in "${OCI8[@]}"; do
         (cd resources/docs/laravel-oci8/$v && git pull)
     else
         echo "Cloning $v..."
-        git clone --single-branch --branch "$v" git@github.com:yajra/laravel-oci8-docs.git "resources/docs/laravel-oci8/$v"
+        git clone --single-branch --branch "$v" https://github.com/yajra/laravel-oci8-docs "resources/docs/laravel-oci8/$v"
     fi;
 done
 
@@ -67,6 +67,6 @@ for v in "${AUDITABLE[@]}"; do
         (cd resources/docs/laravel-auditable/$v && git pull)
     else
         echo "Cloning $v..."
-        git clone --single-branch --branch "$v" git@github.com:yajra/laravel-auditable-docs.git "resources/docs/laravel-auditable/$v"
+        git clone --single-branch --branch "$v" https://github.com/yajra/laravel-auditable-docs "resources/docs/laravel-auditable/$v"
     fi;
 done
