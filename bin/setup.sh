c092214159c2e1e7f7eeb33193d1b903d01bd602
Arjay Angeles <aqangeles@gmail.com>
2022-10-13 15:02:20 +0800

    chore: migrate on setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,6 +9,7 @@ composer install
 touch database/database.sqlite
 cp .env.example .env
 php artisan key:generate
+php artisan migrate
 source "$(dirname "$0")/checkout_latest_docs.sh"
 npm install
 npm run dev
