f90d5e7703d68aacb87295d16a1ca1909965cde4
Arjay Angeles <aqangeles@gmail.com>
2022-10-19 13:08:45 +0800

    ui: component

--- resources/views/docs/partials/sidebar.blade.php
+++ resources/views/docs/partials/sidebar.blade.php
@@ -19,21 +19,7 @@
                 <div class="docs_sidebar">
                     {!! $index !!}
 
-                    @production
-{{--                    <x-ads ad-slot="5965792075" />--}}
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2399525660597307"
-                            crossorigin="anonymous"></script>
-                    <!-- responsive-vertical -->
-                    <ins class="adsbygoogle"
-                         style="display:block"
-                         data-ad-client="ca-pub-2399525660597307"
-                         data-ad-slot="6510738472"
-                         data-ad-format="auto"
-                         data-full-width-responsive="true"></ins>
-                    <script>
-                        (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                    @endproduction
+                    <x-ads ad-slot="5965792075" />
                 </div>
             </nav>
         </div>
