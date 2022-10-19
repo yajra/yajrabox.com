c8cb4ff3db445893d315fe57de012e57c029f541
Arjay Angeles <aqangeles@gmail.com>
2022-10-19 11:01:51 +0800

    chore: ads on prod

--- resources/views/docs/show.blade.php
+++ resources/views/docs/show.blade.php
@@ -148,7 +148,6 @@
 
                             <x-accessibility.main-content-wrapper>
                                 {!! $content !!}
-                                {{-- <x-ads ad-slot="6524198363" />--}}
                             </x-accessibility.main-content-wrapper>
                         </section>
                     </section>
