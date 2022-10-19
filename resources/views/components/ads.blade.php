a9d45ac39c9dd312eeeeeb3a2e1566690ce75cba
Arjay Angeles <aqangeles@gmail.com>
2022-10-19 13:09:38 +0800

    ui: remove container

--- resources/views/components/ads.blade.php
+++ resources/views/components/ads.blade.php
@@ -1,18 +1,16 @@
 @props(['adSlot', 'publisher' => 'ca-pub-2399525660597307', 'type' => 'auto'])
 
 @production
-    <div class="mt-4 px-3 py-2 border-dashed border-gray-200 border rounded-lg text-xs leading-loose text-gray-700 lg:block dark:border-gray-400 dark:text-gray-200" {{ $attributes->exceptProps(['adSlot', 'publisher', 'type']) }}>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2399525660597307"
-                crossorigin="anonymous"></script>
-        <!-- responsive-vertical -->
-        <ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-2399525660597307"
-             data-ad-slot="{{ $adSlot }}"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    </div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2399525660597307"
+            crossorigin="anonymous"></script>
+    <!-- responsive-vertical -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-2399525660597307"
+         data-ad-slot="{{ $adSlot }}"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 @endproduction
