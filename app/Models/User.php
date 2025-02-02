1b7205a29a1d08298b95fcdc9f262f1a5a6572a4
Arjay Angeles <aqangeles@gmail.com>
2025-02-02 13:51:00 +0800

    chore: remove jetstream

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,15 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-    use HasProfilePhoto;
-    use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
